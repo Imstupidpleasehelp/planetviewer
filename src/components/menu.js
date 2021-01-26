06bd83ae9c65c6283389ef1d7ecb05ec90a2464c
dylan <dylanabozarth@gmail.com>
2021-01-26 09:11:22 -0800

    props and views

--- src/components/menu.js
+++ src/components/menu.js
@@ -1,11 +1,13 @@
 import React from 'react'
+import { Earth } from './planets/Earf'
 import { PlanetView } from './planetview'
 
 
 export const Menu = () => {
     return (
         <div className="menuMaster">
-            <PlanetView Name="Ass planet" Population="your mom" />
+            
+            <Earth />
         </div>
     )
 }
--- src/components/planets/Earf.jsx
+++ src/components/planets/Earf.jsx
@@ -1,15 +1,17 @@
-import React from 'react'
-
-
+import React from "react";
+import { PlanetView } from "../planetview";
 
 export const Earth = () => {
-    return(
-    <div className="planet">
-          <div className="wrap">
-            <div className="background"></div>
-            <div className="clouds"></div>
-          </div>
-          <div className="mask"></div>
+  return (
+    <div className="planetWrapper">
+      <PlanetView Name="Earth" Population="Many" />
+      <div className="planet">
+        <div className="wrap">
+          <div className="background"></div>
+          <div className="clouds"></div>
         </div>
-    )
-}
+        <div className="mask"></div>
+      </div>
+    </div>
+  );
+};
