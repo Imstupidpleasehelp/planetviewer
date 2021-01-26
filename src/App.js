3dcdb78513cac0542937369fe696366697a669df
dylan <dylanabozarth@gmail.com>
2021-01-25 21:22:34 -0800

    added earf

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import { Earth } from './components/planets/Earf';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       <Earth />
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/planets/Earf.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+
+
+export const Earth = () => {
+    return(
+    <div className="planet">
+          <div className="wrap">
+            <div className="background"></div>
+            <div className="clouds"></div>
+          </div>
+          <div className="mask"></div>
+        </div>
+    )
+}
