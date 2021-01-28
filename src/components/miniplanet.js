ab339a41aba9a7431972a106582855c26116cf50
dylan <dylanabozarth@gmail.com>
2021-01-28 14:26:06 -0800

    mini planet class

--- src/components/miniplanet.js
+++ src/components/miniplanet.js
@@ -4,7 +4,7 @@ import { NavLink } from "react-router-dom";
 export const MiniPlanet = (props) => {
   return (
     <NavLink to={props.name}>
-      <div className=`miniPlanet ${props.className}`></div>
+      <div className={`miniPlanet ${props.className}`}></div>
 
      
     </NavLink>
