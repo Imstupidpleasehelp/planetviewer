bc723d7826ed9ea7ebbffb57f1925934e9a393d3
dylan <dylanabozarth@gmail.com>
2021-01-31 19:17:35 -0800

    with background

--- src/App.js
+++ src/App.js
@@ -3,10 +3,30 @@ import './App.css';
 import { Menu } from './components/menu';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { Earth } from './components/planets/Earf';
-
+import ParticlesBg from 'particles-bg'
 function App() {
+  let config = {
+    num: [15, 20],
+    rps: 0.5,
+    radius: [1, 1.5],
+    life: [6, 3],
+    v: [2, 3],
+   
+    // body: "./img/icon.png", // Whether to render pictures
+    // rotate: [0, 20],
+    alpha: [0.6, 0],
+    scale: [1, 0.1],
+    position: "all", // all or center or {x:1,y:1,width:100,height:100}
+    color: ["#FFFFFF"],
+    cross: "dead", // cross or bround
+    random: 2,  // or null,
+    g: 1,    // gravity
+    // f: [2, -1], // force
+    
+  };
   return (
     <Router>
+      <ParticlesBg color="#FFF" className="background" type="custom" config={config} bg={true} />
     <div className="App">
       <Route exact path="/" component={Menu} />
     </div>
