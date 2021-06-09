712c8972cf164abf32e136ee1cc70e1a31251e88
dylan <dylanabozarth@gmail.com>
2021-06-09 16:37:10 -0700

    starting orbit

--- src/components/solsystem.js
+++ src/components/solsystem.js
@@ -5,8 +5,12 @@ import { MiniPlanet } from "../components/miniplanet";
 
 export const SolSystem = (props) => {
     useEffect(() => {
-props.setBar("Sol System")
-    }, [])
+        props.setBar("Sol System")
+        Orbit();
+    })
+    const Orbit = () => {
+        gsap.to(".sol-system-master", {rotation: 360, x: 100, y: 100, duration: 5})
+    }
     return (
         <div className="sol-system-master">
             
@@ -15,9 +19,17 @@ props.setBar("Sol System")
        // onMouseOver={() => props.setBar("Yellow Dwarf Star")}
        
       ></div>
- <MiniPlanet name="mercury" className=" mercuryMini " />
+      <div //onMouseOver={() => props.setBar("Barren World")}
+      //onMouseLeave={() => props.setBar('Sol System')}
+        >
+ <MiniPlanet name="mercury"  className=" mercuryMini " /></div>
  <MiniPlanet name="venus" className=" venusMini " />
  <MiniPlanet name="earth" className=" earthMini " />
+ <MiniPlanet name="mars" className=" marsMini " />
+ <MiniPlanet name="juipter" className=" juipterMini " />
+ <MiniPlanet name="saturn" className=" saturnMini " />
+ <MiniPlanet name="uranus" className=" uranusMini " />
+ <MiniPlanet name="neptune" className="neptuneMini " />
         </div>
     )
 }
