2580b2a18d275773c753ede0b7bee4c7db6ea7a4
dylan <dylanabozarth@gmail.com>
2021-06-11 13:58:07 -0700

    random numbers 2

--- src/interstellar-view/interstellar.jsx
+++ src/interstellar-view/interstellar.jsx
@@ -4,16 +4,32 @@ import { gsap } from "gsap";
 import { useEffect } from "react";
 export const Interstellar = () => {
   useEffect(() => {
-   
+   makeid()
+   RandomStarNum()
   });
   //function stuff() {
     //gsap.to(".yellow-dwarf", { rotation: 27, x: 1000, duration: 15 });
   //}
   function makeid(length) {
     let r = Math.random().toString(36).substring(7);
+    
     console.log(r)
+    return r
    }
-   
+   function RandomStarNum() {
+    var num = Math.floor(Math.random() * 7 + 1);
+    console.log(num)
+    return num
+   }
+   const starList = {
+    1: 'red giant',
+    2: 'red supergiant',
+    3: 'blue giant',
+    4: 'white dwarf',
+    5: 'yellow dwarf',
+    6: 'red dwarf',
+    7: 'brown dwarf'
+   }
 
  const makeStars = () => {
 
