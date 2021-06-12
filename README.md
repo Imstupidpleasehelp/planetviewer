ed699b7d4579a8cca05563c76dd8a3582c129d92
dylan <dylanabozarth@gmail.com>
2021-06-12 13:50:42 -0700

    begin nav

--- README.md
+++ README.md
@@ -2,7 +2,7 @@ Work in progress
 things to do.
 1. position of interstellar star labels 
 2. auto generate stars in interstellar view X 
-randomly place them
+randomly place them X
 3. good navigation ui
 4. generate systems for the stars 
 5. orbit animations
--- src/App.js
+++ src/App.js
@@ -4,11 +4,12 @@ import './App.css';
 import { Switch, Route} from "react-router-dom";
 import { Interstellar } from './interstellar-view/interstellar';
 import { SolSystem } from './systems/sol/solsystem';
-
+import {Navigation} from './components/navigation'
 function App() {
   return (
     <div className="App">
-      <Switch >
+      <Navigation /> <Switch >
+       
       <Route exact path="/" component={Interstellar} />
         <Route exact path="/sol" component={SolSystem} />
       </Switch>
