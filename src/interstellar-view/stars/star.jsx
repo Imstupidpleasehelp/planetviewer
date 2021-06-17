9e0398e072f300ab19447c88302495360ab2cdc2
dylan <dylanabozarth@gmail.com>
2021-06-16 20:56:41 -0700

    css bug gone

--- src/interstellar-view/stars/star.jsx
+++ src/interstellar-view/stars/star.jsx
@@ -3,7 +3,7 @@ import "./stars.css";
 import { NavLink } from "react-router-dom";
 export const Star = (props) => {
   return (
-    <NavLink to={props.name}>
+    <NavLink to={`/${props.name}?starType=${props.starType}`}>
       
       <div className={``}>
         <div className={`starBall ${props.starType}`}><p className={`star-label`}>{props.name}</p></div>
--- src/systems/generatesystem.js
+++ src/systems/generatesystem.js
@@ -1,10 +1,19 @@
 import  "./generatesystem.css"
-import {useEffect} from 'react' 
-import { SystemStar } from "../interstellar-view/stars/star"
+import React, {useEffect} from "react";
+import { Link, useLocation, useParams } from "react-router-dom";
+
+function useQuery() {
+  return new URLSearchParams(useLocation().search);
+}
+
 export const GenerateSystem = (props) => {
     useEffect(() => {
-console.log(props)
+        console.log(splitCss)
     })
-    return(<div className={props.starType}>System 
-    <p>{props.name}</p></div>)
+    const {name} = useParams();
+    
+    const query = useQuery();
+    const starType = query.get('starType')
+const splitCss = starType.split(' ')
+    return(<div className="generatedSystem"><div className={splitCss[0] +'1'}></div> </div>)
 }
