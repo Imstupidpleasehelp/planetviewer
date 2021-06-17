08088cac7449fe3bbc853b578c400ce673de15d4
dylan <dylanabozarth@gmail.com>
2021-06-17 09:28:17 -0700

    now they are centered

--- src/systems/generatesystem.js
+++ src/systems/generatesystem.js
@@ -1,5 +1,5 @@
-import  "./generatesystem.css"
-import React, {useEffect} from "react";
+import "./generatesystem.css";
+import React, { useEffect } from "react";
 import { Link, useLocation, useParams } from "react-router-dom";
 
 function useQuery() {
@@ -7,13 +7,18 @@ function useQuery() {
 }
 
 export const GenerateSystem = (props) => {
-    useEffect(() => {
-        console.log(splitCss)
-    })
-    const {name} = useParams();
-    
-    const query = useQuery();
-    const starType = query.get('starType')
-const splitCss = starType.split(' ')
-    return(<div className="generatedSystem"><div className={splitCss[0] +'1'}></div> </div>)
-}
+  useEffect(() => {
+    console.log(splitCss);
+  });
+  const { name } = useParams();
+
+  const query = useQuery();
+  const starType = query.get("starType");
+  const splitCss = starType.split(" ");
+  return (
+    <div className="generatedSystem">
+        <Link to="/">Back to space</Link>
+      <div className={splitCss[0] + "1"}></div>{" "}
+    </div>
+  );
+};
