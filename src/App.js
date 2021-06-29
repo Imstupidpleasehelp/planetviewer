c3b9b817e751e2e55b53b658561ffcd660175e98
dylan <dylanabozarth@gmail.com>
2021-06-28 17:00:57 -0700

    begin chassis

--- src/App.js
+++ src/App.js
@@ -43,20 +43,25 @@ export const Navigation = () => {
     setOpen("navbar-closed");
   };
   return (
-    <div className={`container-fluid`}>
-      <div className="button-container">
-        {open === "navbar-open" ? (
-          <button onClick={() => closeNav()} className="button btn-success">make small</button>
-        ) : (
-          <button onClick={() => openNav()} className="button btn-success">
-            Make big
-          </button>
-        )}
-      </div>
-      <div className={`navbar ${open}`}>
-        <div className="nav-item"><NavLink to="/">Intersellar Space</NavLink></div>
-        <div className="scan-bar">{Scan}</div>
-      </div>
-    </div>
+
+     <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">Navbar</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <NavLink to="/">Interstellar Space</NavLink>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Link</a>
+      
+      </li>
+    </ul>
+   
+  </div>
+</nav>
   );
 };
