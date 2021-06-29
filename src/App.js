b33637b5d8e43cf8708558b082fe4ad26d3ced26
dylan <dylanabozarth@gmail.com>
2021-06-28 17:07:20 -0700

    collapse animation

--- src/App.js
+++ src/App.js
@@ -35,7 +35,7 @@ export default App;
 
 export const Navigation = () => {
   const [Scan, setScan] = useState("");
-  const [open, setOpen] = useState("navbar-closed");
+  const [open, setOpen] = useState("navbar-open");
   const openNav = () => {
     setOpen("navbar-open");
   };
@@ -44,15 +44,19 @@ export const Navigation = () => {
   };
   return (
 
-     <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">Navbar</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+     <nav class={`navbar navbar-expand-lg navbar-light bg-light ${open}`}>
+  <div className='text-center'>
+  <div className="button-container">
+        {open === "navbar-open" ? (
+          <button onClick={() => closeNav()} className="button btn-success">make small</button>
+        ) : (
+          <button onClick={() => openNav()} className="button btn-success">
+            Make big
+          </button>
+        )}
+      </div>
     <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
+      <li class="nav-item ">
         <NavLink to="/">Interstellar Space</NavLink>
       </li>
       <li class="nav-item">
@@ -60,7 +64,7 @@ export const Navigation = () => {
       
       </li>
     </ul>
-   
+  
   </div>
 </nav>
   );
